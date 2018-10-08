d48e072124d4a03ac3f36ac76bf57e4bb8772c5c
HeyItzGinger <league@WTS14.attlocal.net>
2018-10-08 16:55:11 -0700

    Guest Book (ip)

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -2,9 +2,11 @@ package intro_to_array_lists;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class GuestBook implements ActionListener {
@@ -12,8 +14,12 @@ public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other
 	// button reads "View Names".
 	public static void main(String[] args) {
-
+		GuestBook gb = new GuestBook();
 	}
+
+	JButton button1 = new JButton("add name");
+	JButton button2 = new JButton("view names");
+	ArrayList<String> list = new ArrayList<String>();
 
 	// When the add name button is clicked, display an input dialog that asks the
 	// user to enter a name. Add
@@ -22,11 +28,11 @@ public class GuestBook implements ActionListener {
 		JPanel panel = new JPanel();
 		frame.setVisible(true);
 		frame.add(panel);
-		JButton button1 = new JButton("add name");
-		JButton button2 = new JButton("view names");
 		panel.add(button1);
-		panel.add(button2);button1.addActionListener(this);
-	
+		panel.add(button2);
+		button1.addActionListener(this);
+	}
+
 	// that name to an ArrayList. When the "View Names" button is clicked, display a
 	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -37,9 +43,15 @@ public class GuestBook implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(button1 == e) {
-			
+		if (button1 == e.getSource()) {
+			list.add(JOptionPane.showInputDialog("Please enter a name."));
+		}
+		if (button2 == e.getSource()) {
+			for (int i = 0; i < list.size(); i++) {
+
+				JOptionPane.showMessageDialog(null, "");
+			}
 		}
 	}
-	}
+
 }
